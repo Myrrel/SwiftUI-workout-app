eb4cbc7763ccaeecd8196d1f1e35d49f49ee97af
Martin Urciuoli <myrrel@gmail.com>
2025-02-08 21:22:01 -0300

    pushing initial project

---
+++ workout-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  workout-app
+//
+//  Created by martin on 08/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ workout-app/workout_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  workout_appApp.swift
+//  workout-app
+//
+//  Created by martin on 08/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct workout_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
